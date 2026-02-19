45f69c36539a9d1d4b17b29f97e46daa11114407
jakeblackburnn <jackblackburnn@icloud.com>
2026-02-19 14:28:35 -0500

    Add avg ingredient value experiment

--- run_mc.py
+++ run_mc.py
@@ -1,14 +1,15 @@
 from monte_carlo.runner import MonteCarlo, MonteCarloConfig, Experiment
 from monte_carlo.experiments.easy_seven import EasyExperiment, EasyResult
+from monte_carlo.experiments.avg_ingredient_value import AverageIngredientPerformance, AverageIngredientResult
+from src.database import IngredientsDatabase
 
 # set up experimental config
 # should maybe be a way to this in oneline?
 # MonteCarlo classmethod?
-config = MonteCarloConfig(num_simulations=100000, progress_bar=True)
-result = EasyResult(config=config)
-exp    = EasyExperiment(inv_size=3)
+db     = IngredientsDatabase()
+config = MonteCarloConfig(num_simulations=64000, progress_bar=True)
+result = AverageIngredientResult(config=config, db=db)
+exp    = AverageIngredientPerformance(db=db, inv_size=32)
 
 runner = MonteCarlo(config, result, verbose=True)
 runner.run(exp)
-
-print(result.to_dataframe())
